0fab894d3a040ef75ff02359ebd07688924ab9a0
Michal Ciberej <michal.ciberej@gmail.com>
2023-05-25 21:58:12 +0200

    Create event Listeners on every button

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-let container = document.querySelector("#squaresContainer");
+let color = "";
+const container = document.querySelector("#squaresContainer");
+const colorPicker = document.querySelector("#colorPicker");
+const eraser = document.querySelector("#eraser");
+const rainbow = document.querySelector("#colorRainbow");
+const smallSizeSquares = document.querySelector("#smallSizeSquares");
+const mediumSizeSquares = document.querySelector("#mediumSizeSquares");
+const largeSizeSquares = document.querySelector("#largeSizeSquares");
+const maxSizeSquares = document.querySelector("#maxSizeSquares");
+
+colorPicker.addEventListener("change", (event) => {
+    color = event.target.value;
+});
+eraser.addEventListener("click", (event) => {
+
+});
+rainbow.addEventListener("click", (event) => {
+
+});
+smallSizeSquares.addEventListener("click", () => {
+    createSquares(8, 8);
+    container.querySelectorAll("div").classList.add("mediumSquares");
+    smallSizeSquares.classList.toggle("active");
+})
+
+mediumSizeSquares.addEventListener("click", () => {
+    createSquares(16, 16)
+    mediumSizeSquares.classList.toggle("active")
+})
+
+largeSizeSquares.addEventListener("click", () => {
+    createSquares(32, 32)
+    largeSizeSquares.classList.toggle("active")
+})
+    
+maxSizeSquares.addEventListener("click", () => {
+    createSquares(64,64);
+    maxSizeSquares.classList.toggle("active");
+});
+//square.addEventListener("hovering", () => );
 
 function square() {
     const square = document.createElement("div");
-    square.classList.add("squares")
-    container.appendChild(square)
+    square.classList.add("squares");
+    container.appendChild(square);
 }
 
 function createSquares(row, column) {
